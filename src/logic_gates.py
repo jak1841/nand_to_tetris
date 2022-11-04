8f4087c9a7b14b8f48187a5c130ee553c7d4612d
jak1841 <jaskarnd16@gmail.com>
2022-11-04 12:01:13 -0500

    first commit

---
+++ src/logic_gates.py
@@ -0,0 +1,18 @@
+
+class gate:
+    def __init__(self):
+        pass
+
+    # Given a 2 single bit both in the form of string, returns a bit speceficed by nand operation
+    def nand(self, a, b):
+        if (a == "1" and b == "1"):
+            return "0"
+        return "1"
+
+def main():
+    print("start of a journey")
+    pass
+
+main()
+
+
