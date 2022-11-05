4b1d1b63ffbf924407cb5cfaf42d5760998dab9f
jak1841 <jaskarnd16@gmail.com>
2022-11-05 17:15:47 -0500

    Test, Implementation of Nand Gate

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 run: src/main.py
 	python3 src/main.py
+
+run_test: test
+	python3 test/test_gates.py
---
+++ test/test_gates.py
@@ -0,0 +1,21 @@
+import unittest
+import sys
+
+# allows use of
+sys.path.append('/mnt/c/Users/Jaskarn Dhillon/projects/nand_to_tetris/src')
+from logic_gates import gate as g
+
+gate = g()
+
+class Test(unittest.TestCase):
+
+    def test_nand(self):
+        self.assertEqual("0", gate.nand("1", "1"))
+        self.assertEqual("1", gate.nand("1", "0"))
+        self.assertEqual("1", gate.nand("0", "1"))
+        self.assertEqual("1", gate.nand("0", "0"))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
