e18979ad3a21b7613b47a078d5ea71bce0938cf7
jak1841 <jaskarnd16@gmail.com>
2022-12-01 16:20:21 -0600

    Test Implement Ram_n_bit

--- src/random_access_memory.py
+++ src/random_access_memory.py
@@ -8,8 +8,9 @@ gate = g()
 # Given an n, n = s^x for some n implements the RAM part of the computer
 # Where n is the number of registers in the operation
 class Ram_n:
-    def __init__(self, N):
-        init_registers_array(N, 16)
+    def __init__(self, N, width):
+        self.width = width
+        self.init_registers_array(N, self.width)
 
     # Makes N registers with width:= width and puts it into registers_array 
     # All registers will be be initialized to 0. 
@@ -19,7 +20,7 @@ class Ram_n:
             zero+="0"
 
         self.registers_array = []
-        for x in range(n):
+        for x in range(N):
             new_register = seq()
             new_register.register_n_bit(zero, 1)
             self.registers_array.append(new_register)
@@ -29,6 +30,43 @@ class Ram_n:
     # Function:     output(t) = Ram[address(t)](t)
     #               if load(t - 1) then 
     #                   Ram[address(t - 1)](t) = in (t - 1)
-    
+    def do_operation(self, input, address, load):
+        # Goes through and produces 2^(len(address)) inputs with load bit prepended
+        # To be used for all list_registers as input with load bit
+        new_input = load + input
+        list_inputs = gate.n_bit_n_way_demultiplexor(new_input, address)
+
+        list_register_values = []   # Stores the output of every register value after updating it 
+        
+        # Updates each register with load bit and input from previous code
+        for x in range(len(self.registers_array)):
+            inp = list_inputs[x][1:]
+            load_bit = list_inputs[x][0]
+            list_register_values.append(self.registers_array[x].register_n_bit(inp, load_bit))
+        
+        # Takes register values and returns one based off of its address 
+        return gate.n_bit_n_way_multiplexor(list_register_values, address)
+        
+        
+    def get_list_of_all_register_values(self):
+        zero = ""
+        for x in range(self.width):
+            zero += "0"
+
+        temp = []
+        for x in range(len(self.registers_array)):
+            temp.append(self.registers_array[x].register_n_bit(zero, 0))
+        
+        return temp
+            
+
+
+
+        
+
+            
+
+
+
         
         
--- test/test_sequential_logic.py
+++ test/test_sequential_logic.py
@@ -4,6 +4,7 @@ import sys
 # allows use of moducles
 sys.path.append('/mnt/c/Users/Jaskarn Dhillon/projects/nand_to_tetris/src')
 from logic_gates import sequential as seq
+from random_access_memory import Ram_n
 
 class Test(unittest.TestCase):
 
@@ -45,6 +46,59 @@ class Test(unittest.TestCase):
         self.assertEqual("0100001000000000", s.register_n_bit("0100000000000000", "0"))
         self.assertEqual("0100000000000000", s.register_n_bit("0100000000000000", "1"))
 
+    def test_ram_n(self):
+        ram = Ram_n(8, 4)
+
+        self.assertEqual(["0000", "0000", "0000", "0000", "0000", "0000", "0000", "0000" ], ram.get_list_of_all_register_values())
+        self.assertEqual("0000", ram.do_operation("1111", "000", "0"))
+        self.assertEqual(["0000", "0000", "0000", "0000", "0000", "0000", "0000", "0000" ], ram.get_list_of_all_register_values())
+        self.assertEqual("1111", ram.do_operation("1111", "000", "1"))
+        self.assertEqual(["1111", "0000", "0000", "0000", "0000", "0000", "0000", "0000" ], ram.get_list_of_all_register_values())
+        self.assertEqual("1010", ram.do_operation("1010", "100", "1"))
+        self.assertEqual(["1111", "0000", "0000", "0000", "1010", "0000", "0000", "0000" ], ram.get_list_of_all_register_values())
+
+        ram = Ram_n(16, 8)
+        self.assertEqual([  "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", 
+                            "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000"  ], ram.get_list_of_all_register_values())
+        self.assertEqual("00100110", ram.do_operation("00100110", "1000", "1"))
+        self.assertEqual([  "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", 
+                            "00100110", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000"  ], ram.get_list_of_all_register_values())
+
+        self.assertEqual("01111111", ram.do_operation("01111111", "1000", "1"))
+        self.assertEqual([  "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", 
+                            "01111111", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000"  ], ram.get_list_of_all_register_values())
+
+        self.assertEqual("11001110", ram.do_operation("11001110", "1111", "1"))
+        self.assertEqual([  "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", 
+                            "01111111", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "11001110"  ], ram.get_list_of_all_register_values())
+
+        self.assertEqual("10101111", ram.do_operation("10101111", "0000", "1"))
+        self.assertEqual([  "10101111", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", 
+                            "01111111", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "11001110"  ], ram.get_list_of_all_register_values())
+
+        self.assertEqual("00001110", ram.do_operation("00001110", "1011", "1"))
+        self.assertEqual([  "10101111", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", 
+                            "01111111", "00000000", "00000000", "00001110", "00000000", "00000000", "00000000", "11001110"  ], ram.get_list_of_all_register_values())
+
+        self.assertEqual("01001110", ram.do_operation("01001110", "1110", "1"))
+        self.assertEqual([  "10101111", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", 
+                            "01111111", "00000000", "00000000", "00001110", "00000000", "00000000", "01001110", "11001110"  ], ram.get_list_of_all_register_values())
+
+
+        # Reading 
+        self.assertEqual("10101111", ram.do_operation("00000000", "0000", "0"))
+        self.assertEqual("00000000", ram.do_operation("00000000", "0101", "0"))
+        self.assertEqual("01111111", ram.do_operation("00000000", "1000", "0"))
+        self.assertEqual("00001110", ram.do_operation("00000000", "1011", "0"))
+        self.assertEqual("11001110", ram.do_operation("00000000", "1111", "0"))
+        self.assertEqual("01001110", ram.do_operation("00000000", "1110", "0"))
+
+        self.assertEqual([  "10101111", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", "00000000", 
+                            "01111111", "00000000", "00000000", "00001110", "00000000", "00000000", "01001110", "11001110"  ], ram.get_list_of_all_register_values())
+
+
+
+        
 
 
 
